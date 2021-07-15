2e4433fbf153aa74e82a536622295b3f6f4f0bbf
Cristian Barbosa <cristianbr7@live.com>
2021-07-15 11:00:31 -0500

    Api Gamificacion OK

--- horus_prueba/ViewModels/GamificationViewModel.cs
+++ horus_prueba/ViewModels/GamificationViewModel.cs
@@ -1,24 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using horus_prueba.Models.Challenges;
+using horus_prueba.Services.Request;
 using Prism.Commands;
 using Prism.Navigation;
+using Xamarin.Forms;
 
 namespace horus_prueba.ViewModels
 {
     public class GamificationViewModel : BaseViewModel
     {
-        #region Attributes
+        #region /Attributes
+        private IApiManager _apiManager;
         private INavigationService _navigationService;
-        #endregion Attributes
+
+        private ObservableCollection<ChallengeModel> _challenges;
+        #endregion /Attributes
+
+        #region Properties
+        public ObservableCollection<ChallengeModel> Challenges
+        {
+            get => _challenges;
+            set => SetProperty(ref _challenges, value);
+        }
+        #endregion Properties
 
         #region Properties
         public ICommand SingOutCommand => new DelegateCommand(OnSingOutCommand);
         #endregion Properties
 
         #region Constructor
-        public GamificationViewModel(INavigationService navigationService)
+        public GamificationViewModel(IApiManager apiManager, INavigationService navigationService)
         {
+            // Variables
+            this._apiManager = apiManager;
             this._navigationService = navigationService;
+            this.Challenges = new ObservableCollection<ChallengeModel>();
+
+            // Commands
+            RefreshCommand = new Command(async () => await RunSafe(InitGamification(), ShowLoading: false));
         }
         #endregion Constructor
 
@@ -26,6 +49,23 @@ namespace horus_prueba.ViewModels
         #endregion Methods
 
         #region Commands
+        private async Task InitGamification()
+        {
+            // Eventos Iniciales
+            await Task.Delay(250);
+            this.Challenges.Clear();
+
+            // API
+            var challengeApi = await this._apiManager.GetChallenges();
+            if (!challengeApi.IsSuccess)
+            {
+                PageDialog.Alert(challengeApi.Message, "Error", "Aceptar");
+                return;
+            }
+            var challenges = challengeApi.Response as List<ChallengeModel>;
+            this.Challenges = new ObservableCollection<ChallengeModel>(challenges);
+        }
+
         /// <summary>
         /// Comando para cerrar sesión
         /// </summary>
@@ -38,6 +78,14 @@ namespace horus_prueba.ViewModels
         #endregion Commands
 
         #region Navigation
+        public override void OnAppearing()
+        {
+            if (!this.IsInit)
+                return;
+
+            this.IsRefreshing = true;
+            this.IsInit = false;
+        }
         #endregion Navigation
     }
 }
