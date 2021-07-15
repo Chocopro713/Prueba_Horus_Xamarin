de4c119e300a96c9cde8ae8f76e6a98bcd0030c8
Cristian Barbosa <Cristianbr7@live.com>
2021-07-15 09:03:39 -0500

    Realizando procesos

--- horus_prueba/ViewModels/GamificationViewModel.cs
+++ horus_prueba/ViewModels/GamificationViewModel.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Navigation;
+
 namespace horus_prueba.ViewModels
 {
     public class GamificationViewModel
     {
-        public GamificationViewModel()
+        #region Attributes
+        private INavigationService _navigationService;
+        #endregion Attributes
+
+        #region Properties
+        public ICommand SingOutCommand => new DelegateCommand(OnSingOutCommand);
+        #endregion Properties
+
+        #region Constructor
+        public GamificationViewModel(INavigationService navigationService)
         {
+            this._navigationService = navigationService;
         }
+        #endregion Constructor
+
+        #region Methods
+        #endregion Methods
+
+        #region Commands
+        /// <summary>
+        /// Comando para cerrar sesión
+        /// </summary>
+        private void OnSingOutCommand()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Commands
+
+        #region Navigation
+        #endregion Navigation
     }
 }
--- horus_prueba/ViewModels/LoginViewModel.cs
+++ horus_prueba/ViewModels/LoginViewModel.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Navigation;
+
 namespace horus_prueba.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
-        public LoginViewModel()
+        #region Attributes
+        private INavigationService _navigationService;
+        #endregion Attributes
+
+        #region Properties
+        public ICommand LoginCommand => new DelegateCommand(OnLoginCommand);
+        #endregion Properties
+
+        #region Constructor
+        public LoginViewModel(INavigationService navigationService)
         {
+            this._navigationService = navigationService;
         }
+        #endregion Constructor
+
+        #region Methods
+        #endregion Methods
+
+        #region Commands
+        /// <summary>
+        /// Validación para iniciar sesión
+        /// </summary>
+        private async void OnLoginCommand()
+        {
+            var navigation = await this._navigationService.NavigateAsync(new Uri("GamificationPage", UriKind.Absolute));
+            if (!navigation.Success)
+                PageDialog.Alert($"{navigation.Exception}");
+        }
+        #endregion Commands
+
+        #region Navigation
+        #endregion Navigation
     }
 }
